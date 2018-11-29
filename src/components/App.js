5c811be2051f22dfde3911dfb8fb61559f1ce40c
Dusan Rasic <dusanrasic@spinviewglobal.com>
2018-11-29 09:44:11 +0100

    initial setup

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import '../style/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className='el-wrapper'>
+          
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
