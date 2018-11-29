c77b0327d1db2c67d1c091f43194c483861c1d10
Dusan Rasic <dusanrasic@spinviewglobal.com>
2018-11-29 11:15:12 +0100

    adding sass/webpack-notifier

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-import '../style/App.css';
+import '../style/App.scss';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className='el-wrapper'>
-          
         </div>
       </div>
     );
