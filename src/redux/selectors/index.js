e3bd74ee505783473b6eef7e9e43c0b9ac47c559
Dusan Rasic <dusanrasic@spinviewglobal.com>
2018-12-07 12:41:16 +0100

    responsive styles

--- src/redux/selectors/index.js
+++ src/redux/selectors/index.js
@@ -8,13 +8,10 @@ export const getVisibleTodos = createSelector(
   (visibilityFilter, todos) => {
   switch (visibilityFilter) {
       case 'View All':
-        console.log('VIEW ALL SELECTED');
         return todos
       case 'Active':
-        console.log('ACTIVE SELECTED');
         return todos.filter(t => t.active)
       case 'Completed':
-        console.log('COMPLETED SELECTED');
         return todos.filter(t => !t.active)
       default:
         return todos
